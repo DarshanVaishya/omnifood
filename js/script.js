58cab6b96d16d3d9a802b63049c4c9aae23a477f
DarshanVaishya <DarshanVaishya3@gmail.com>
2022-01-17 11:46:40 +0530

    Fixed issue with link

--- js/script.js
+++ js/script.js
@@ -11,68 +11,67 @@
 
 ///////////////////////////////////////////////////////////
 // Adding year dynamically
-const year = document.querySelector('.year');
+const year = document.querySelector(".year");
 year.textContent = new Date().getFullYear();
 
 ///////////////////////////////////////////////////////////
 // Toggling nav-open class
-const btnNav = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header')
-btnNav.addEventListener('click', function () {
-  headerEl.classList.toggle('nav-open')
-})
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNav.addEventListener("click", function () {
+	headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling
-const allLinks = document.querySelectorAll('a:link');
+const allLinks = document.querySelectorAll("a:link");
 allLinks.forEach((link) => {
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+		const href = link.getAttribute("href");
 
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
+		// Scroll to top
+		if (href === "#") {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		}
 
-    // Scroll to top
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
+		// scroll for other links
+		if (href != "#" && href.startsWith("#")) {
+			const sectionEl = document.querySelector(href);
+			sectionEl.scrollIntoView({ behavior: "smooth" });
+		} else window.open(href);
 
-    // scroll for other links
-    if (href != "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" })
-    }
-
-    // mobile nav
-    if(link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  })
-})
+		// mobile nav
+		if (link.classList.contains("main-nav-link"))
+			headerEl.classList.toggle("nav-open");
+	});
+});
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
+	function (entries) {
+		const ent = entries[0];
+		console.log(ent);
 
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
+		if (ent.isIntersecting === false) {
+			document.body.classList.add("sticky");
+		}
 
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
+		if (ent.isIntersecting === true) {
+			document.body.classList.remove("sticky");
+		}
+	},
+	{
+		// In the viewport
+		root: null,
+		threshold: 0,
+		rootMargin: "-80px",
+	}
 );
 obs.observe(sectionHeroEl);
